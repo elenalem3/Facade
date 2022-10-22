1bbcd6682ab90d875cc77fc371f5f0feb9acfb23
Elena Lemeskina <gogulinaelena@yandex.ru>
2022-10-22 20:21:03 +0300

    Initial commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,13 @@
+public class BinOps {
+    public String sum(String a, String b) {
+        int intA = Integer.parseInt(a, 2);
+        int intB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(intA + intB);
+    }
+
+    public String mult(String a, String b) {
+        int intA = Integer.parseInt(a, 2);
+        int intB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(intA * intB);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "10")); //5+2
+        System.out.println(bins.mult("1010", "111")); //10*7
+    }
+}
